e957207cd011d0b2076d2df0c6cefd8857cbc87e
MCJack123 <jackbruienne@yahoo.com>
2014-03-21 10:04:32 -0400

    Added updater
    Download not available until Monday, March 24, 2014

--- C++Console.cpp
+++ C++Console.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <curl/curl.h>
+#include <curl/types.h>
+#include <curl/easy.h>
 #include "command.h"
+#include "update.h"
 
 int main() {
 	int cont = 1;
@@ -36,6 +40,37 @@ int main() {
 		}
 		else if (commandexe == "help clear") {
 			help("clear");
+		}
+		else if (commandexe == "installer update console") {
+			download("http://www.cppconsole.org/CPPConsole/Latest/.version");
+			std::ifstream ifs(".version");
+  			std::string content;
+			content.assign( (std::istreambuf_iterator<char>(ifs) ),
+                		(std::istreambuf_iterator<char>()    ) );
+                	if (content > "1.1") {
+				#ifdef __APPLE__
+					download("http://www.cppconsole.org/CPPConsole/Latest/C++ConsoleMac");
+					std::stringstream stream;
+					stream <<"update";
+					system(stream.str().c_str());
+				#elif __WIN32__
+					download("http://www.cppconsole.org/CPPConsole/Latest/C++Console.exe");
+					std::stringstream stream;
+					stream <<"update.exe";
+					system(stream.str().c_str());
+				#elif __WIN64__
+					download("http://www.cppconsole.org/CPPConsole/Latest/C++Console.exe");
+					std::stringstream stream;
+					stream <<"update.exe";
+					system(stream.str().c_str());
+				#elif __linux__
+					download("http://www.cppconsole.org/CPPConsole/Latest/Linux/C++ConsoleLinux");
+					std::stringstream stream;
+					stream <<"update";
+					system(stream.str().c_str());
+				#endif
+				
+                	}	
 		}
 		else {
 			std::cout << "The command you have entered has not been defined in command.h, or has not been added to C++Console.cpp.\n";
