e0aad90302e601e0cfb412f9a72ef5cce7d363ed
MCJack123 <jackbruienne@yahoo.com>
2014-03-31 16:16:07 -0400

    add function

--- techStuff.h
+++ techStuff.h
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <string>
+#include <sstring>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+int dumpFile(std::string file) {
+  std::ifstream ifs(file);
+  std::string content;
+	content.assign( (std::istreambuf_iterator<char>(ifs) ),
+    (std::istreambuf_iterator<char>()    ) );
+  return 0;
+}
+int download(std::string url, std::string name) {
+  
+}
