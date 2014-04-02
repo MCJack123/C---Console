c7ac3f9fe5e727baa4c7ec01477f66825ace7ad8
MCJack123 <jackbruienne@yahoo.com>
2014-04-02 16:08:08 -0400

    Update C++Console.cpp

--- C++Console.cpp
+++ C++Console.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <ssring>
 #include "command.h"
+#include "techStuff.h"
+#include "*.h"
 
 int main() {
 	int cont = 1;
-    	std::cout << "C++ Console v1.0\n";
+    	std::cout << "C++ Console v1.1.1\n";
     	while (cont == 1) {
         	std::string commandexe;
         	std::cout << "Console: ";
@@ -37,37 +42,11 @@ int main() {
 		else if (commandexe == "help clear") {
 			help("clear");
 		}
-		else if (commandexe == "installer update console") {
-			std::stringstream stream;
-			stream <<"update"<<"http://www.cppconsole.org/CPPConsole/Latest/.version"<<".version";
-			system(stream.str().c_str());;
-			std::ifstream ifs(".version");
-  			std::string content;
-			content.assign( (std::istreambuf_iterator<char>(ifs) ),
-                		(std::istreambuf_iterator<char>()    ) );
-                	if (content > "1.1") {
-				#ifdef __APPLE__
-					std::stringstream stream;
-					stream <<"update"<<"http://www.cppconsole.org/CPPConsole/Latest/C++ConsoleMac"<<"C++Console";
-					system(stream.str().c_str());
-				#elif __WIN32__
-					std::stringstream stream;
-					stream <<"update.exe"<<"http://www.cppconsole.org/CPPConsole/Latest/C++Console.exe"<<"C++Console.exe";
-					system(stream.str().c_str());
-				#elif __WIN64__
-					std::stringstream stream;
-					stream <<"update.exe"<<"http://www.cppconsole.org/CPPConsole/Latest/C++Console.exe"<<"C++Console.exe";
-					system(stream.str().c_str());
-				#elif __linux__
-					std::stringstream stream;
-					stream <<"update"<<"http://www.cppconsole.org/CPPConsole/Latest/C++ConsoleLinux"<<"C++Console";
-					system(stream.str().c_str());
-				#endif
-				
-                	}	
+		else if (commandexe == "installer") {
+			installer()
 		}
 		else {
-			std::cout << "The command you have entered has not been defined in command.h, or has not been added to C++Console.cpp.\n";
+			std::cout << "The command you have entered has not been defined in command.h, or has not been added to C++Console.\n";
 			}
 		}
 	return 0;
