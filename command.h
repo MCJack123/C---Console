5100964d5ff5e2520bd6dd189389689913a07aff
MCJack123 <jackbruienne@yahoo.com>
2014-04-23 10:12:09 -0400

    Update command.h

--- command.h
+++ command.h
@@ -47,8 +47,8 @@ int clearscreen () {
 	}
 int installer() {
 	std::cout << "---C++ Console Installer---\nSelect an app:\n";
-	//If you know how to list the files/folders of a website, do that for the website cppconsole.bruienne.com.
-	std::cout << ":";
+	list();
+	std::cout << data << ":";
 	std::string app;
 	std::cin >> app;
 	if (app == "CPPConsole) {
@@ -134,3 +134,37 @@ int installer() {
 	}
 	return 0;
 }
+string data; //will hold the url's contents
+
+size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up)
+{ //callback must have this declaration
+    //buf is a pointer to the data that curl has for us
+    //size*nmemb is the size of the buffer
+
+    for (int c = 0; c<size*nmemb; c++)
+    {
+        data.push_back(buf[c]);
+    }
+    return size*nmemb; //tell curl how many bytes we handled
+}
+
+int list()
+{
+    CURL* curl; //our curl object
+
+    curl_global_init(CURL_GLOBAL_ALL); //pretty obvious
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, "http://cppconsole.bruienne.com/");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
+
+    curl_easy_perform(curl);
+
+    cout << endl << data << endl;
+    cin.get();
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    return 0;
